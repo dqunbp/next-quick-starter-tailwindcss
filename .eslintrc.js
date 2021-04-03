662b434e0c84e7fef45696214dfb94ab0a3928c2
dqunbp <dqunbp@gmail.com>
2021-04-03 12:39:56 +0300

    add eslint - add eslint config - add precommit lint:ts hook

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: ["plugin:prettier/recommended"],
+};
